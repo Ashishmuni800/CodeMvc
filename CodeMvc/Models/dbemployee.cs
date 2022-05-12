57cee4037d294e29f5e08864099ad425691be33a
admin <admin@CHETUIWK938>
2022-05-12 18:48:04 +0530

    Add project files.

---
+++ CodeMvc/Models/dbemployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace CodeMvc.Models
+{
+    public class dbemployee:DbContext
+    {
+        public dbemployee() : base("Data Source=CHETUIWK938\\SQLEXPRESS123;Initial Catalog=Watch;Integrated Security=True;Pooling=False") { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
